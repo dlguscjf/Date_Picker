9c78181e2718e0d818e77a19622b055cfb27d93e
D7703_06 <d7703_06@D7703-06ui-iMac.local>
2018-04-11 15:37:42 +0900

    Add Date_picker

--- Date_Picker/ViewController.swift
+++ Date_Picker/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var datetext: UILabel!
+    @IBOutlet weak var datepicker: UIDatePicker!
+    @IBOutlet weak var time: UILabel!
+    var timer = Timer();
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: {(myTimer) in
+            self.updatetime()})
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+
+    @IBAction func datepicker(_ sender: Any) {
+        let dateformatter = DateFormatter()
+        dateformatter.locale = Locale(identifier: "ko_KR")
+        dateformatter.dateFormat="a yyyy-MM-dd EEE"
+        let date = dateformatter.string(from: datepicker.date)
+        datetext.text = date
+       
+        
     }
-
-
+    func updatetime(){
+        let date1 = Date()
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "a HH:mm:ss EEE"
+        let time1 = formatter.string(from: date1)
+        time.text = time1
+    }
 }
 
